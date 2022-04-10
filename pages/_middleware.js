d31129a5168c50f3e79054009253010f9a42aeb9
Antony <ac.chan@live.com>
2022-04-10 06:56:20 +0000

    finishing up middleware request

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,5 +2,21 @@ import { getToken } from 'next-auth/jwt'
 import { nextResponse } from 'next/server'
 
 export async function middleware(req) {
+  // Token will exist if user is logged in
   const token = await getToken({ req, secret: process.env.JWT_SECRET })
+
+  const { pathname } = req.nextUrl
+
+  // Allow the requests if the follow is true...
+  // 1) Its a request for next-auth session & provider fetching
+  // 2) the token exists
+
+  if (pathname.includes('/api/auth') || token) {
+    return nextResponse.next()
+  }
+
+  //  Redirect them to login if they dont have token AND are requesting a protected route
+  if (!token && pathname !== '/login') {
+    return nextResponse.next()
+  }
 }
