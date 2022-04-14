06ff20b99f4377253f2a0cf7578cc9d173d70d0d
Antony <ac.chan@live.com>
2022-04-14 06:57:05 +0000

    import useSession for center

--- components/Center.js
+++ components/Center.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSession } from 'next-auth/react'
 
 export default function Center() {
+  const { date: session } = useSession()
   return (
-    <div className="flex flex-grow">
+    <div className="flex flex-grow text-white">
       <h1>I am center</h1>
+      <header>
+        <div>
+          <img src="session?user.image" alt="" />
+        </div>
+      </header>
     </div>
   )
 }
