d9746f2b3e1e4c175ec42f8cdbce7fdc45992f9c
Antony <ac.chan@live.com>
2022-04-30 05:28:20 +0000

    added chervondownicon

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
+import { ChevronDownIcon } from '@heroicons/react/outline'
 
 export default function Center() {
   const { date: session } = useSession()
@@ -7,8 +8,14 @@ export default function Center() {
     <div className="flex flex-grow text-white">
       <h1>I am center</h1>
       <header>
-        <div>
-          <img src="session?user.image" alt="" />
+        <div className="flex items-center space-x-3 bg-black opacity-90 hover:opacity-80">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={session?.user.image}
+            alt=""
+          />
+          <h2>{session?.user.name}:</h2>
+          <ChevronDownIcon className="h-5 w-5" />
         </div>
       </header>
     </div>
