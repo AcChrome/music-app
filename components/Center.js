1808dbabd1f6ab47accd4164795bc12804bdb3f7
Antony <ac.chan@live.com>
2022-05-06 06:58:46 +0000

    continue styling center

--- components/Center.js
+++ components/Center.js
@@ -7,8 +7,8 @@ export default function Center() {
   return (
     <div className="flex flex-grow text-white">
       <h1>I am center</h1>
-      <header>
-        <div className="flex items-center space-x-3 bg-black opacity-90 hover:opacity-80">
+      <header className="absolute top-5 right-8">
+        <div className="flex cursor-pointer items-center space-x-3 rounded-full bg-black p-1 pr-2 opacity-90 hover:opacity-80">
           <img
             className="h-10 w-10 rounded-full"
             src={session?.user.image}
@@ -18,6 +18,12 @@ export default function Center() {
           <ChevronDownIcon className="h-5 w-5" />
         </div>
       </header>
+      <section
+        className={`from-red=500 padding-8 flex h-80 items-end space-x-7 bg-gradient-to-b to-black text-white`}
+      >
+        <img src="" alt="" />
+        <h1>hello</h1>
+      </section>
     </div>
   )
 }
